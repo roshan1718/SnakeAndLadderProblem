a921ac05d640ad0ff7cc8fef6e1ab17af274c8dd
roshan1718 <roshanshinde1717@gmail.com>
2020-03-21 14:33:25 +0530

    ADDED code to exact winning position

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -6,45 +6,54 @@
 # @ Since: 21-03-2020
 
 #CONSTANT
-STRAT_POSITION=0
+START_POSITION=0
 END_POSITION=100
 
 #variables
 position=0
 
 
+function playGame()
+{
 while [[ $position -lt $END_POSITION ]]
 do
 	diceOutput=$((RANDOM%6+1))
 	choice=$((RANDOM%3))
+
 	case $choice in
 		0)
-			# No Play Condition
+			#No Play Condition
 			if [[ $position -lt 0 ]]
-         		then
-            			position=0
+  			then
+				position=$START_POSITION
 			else
-         			position=$(($position+0))
+      				position=$(($position+0))
 			fi
 			;;
 		1)
-			# Snake
-			if [[ $position -lt 0 ]]
-         		then
-            			position=0
-         		else
-            			position=$(($position-$diceOutput))
-			fi
-			;;
-		*)
-			# ladder
+			#Snake
 			if [[ $position -lt 0 ]]
 			then
-				position=0
+				position=$START_POSITION
 			else
-				position=$(($position+$diceOutput))
+				position=$(($position-$diceOutput))
 			fi
+			;;
+		2)
+			#Ladder
+			if [[ $(($END_POSITION - $position)) -ge $diceOutput ]]
+			then
+				position=$(($position+$diceOutput))
+			else
+				position=$(($position+0))
+			fi
+
+			;;
+		*)
+			echo Invalid
 			;;
 	esac
 done
+}
+playGame
 echo "Player position ::"$position
