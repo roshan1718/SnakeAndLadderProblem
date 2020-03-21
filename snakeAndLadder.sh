7193c76133aefc207b781c20ef9b6867fdaa5a90
roshan1718 <roshanshinde1717@gmail.com>
2020-03-21 15:02:28 +0530

    ADDED code to find position after every die role and total dice count

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -11,9 +11,9 @@ END_POSITION=100
 
 #variables
 position=0
+diceRoll=0
 
-
-function playGame()
+function play()
 {
 while [[ $position -lt $END_POSITION ]]
 do
@@ -24,10 +24,10 @@ do
 		0)
 			#No Play Condition
 			if [[ $position -lt 0 ]]
-  			then
+			then
 				position=$START_POSITION
 			else
-      				position=$(($position+0))
+				position=$(($position+0))
 			fi
 			;;
 		1)
@@ -47,13 +47,15 @@ do
 			else
 				position=$(($position+0))
 			fi
-
 			;;
 		*)
-			echo Invalid
+			echo Invalid choice
 			;;
 	esac
+	((diceRoll++))
+	echo Position After $diceRoll th Roll.. $position
 done
+echo Player Won
 }
-playGame
-echo "Player position ::"$position
+play
+echo Number of Times Dice Roll..$diceRoll
