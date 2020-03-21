289af5c6c7227cba5463e8e9595ef43c634f9d99
roshan1718 <roshanshinde1717@gmail.com>
2020-03-21 13:25:10 +0530

    ADDED code to check player status

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -9,5 +9,23 @@
 STRAT_POSITION=0
 END_POSITION=100
 
+#variables
+position=0
+
 diceOutput=$((RANDOM%6+1))
-echo  $diceOutput
+echo "Diceoutput is ::" $diceOutput
+
+diceOutput=$((RANDOM%6+1))
+choice=$((RANDOM%3))
+	case $choice in
+		0)
+			# No Play Condition
+			position=$(($position+0)) ;;
+		1)
+			# Snake
+			position=$(($position-$diceOutput)) ;;
+		*)
+			# ladder
+			position=$(($position+$diceOutput))	;;
+	esac
+echo "Position of Playar is::"$position
