82891e63b32204a95f65bb6c8cb07c2e1689a66f
roshan1718 <roshanshinde1717@gmail.com>
2020-03-21 12:05:21 +0530

    ADDED code to get diceoutput

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -2,9 +2,12 @@
 
 # @ Description: Snake And Ladder Problem
 # @ Author: Roshan Balkrushna Shinde
-# @ Version: 18.04.3 lts
+
 # @ Since: 21-03-2020
 
 #CONSTANT
 STRAT_POSITION=0
 END_POSITION=100
+
+diceOutput=$((RANDOM%6+1))
+echo  $diceOutput
