42d40e5dc7b38a79fbe8f13633a60817da00ddb3
roshan1718 <roshanshinde1717@gmail.com>
2020-03-21 10:06:59 +0530

    ADDED code to game start with position zero

---
+++ snakeAndLadder.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+# @ Description: Snake And Ladder Problem
+# @ Author: Roshan Balkrushna Shinde
+# @ Version: 18.04.3 lts
+# @ Since: 21-03-2020
+
+#CONSTANT
+STRAT_POSITION=0
+END_POSITION=100
